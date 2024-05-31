cf943c3bfec0ec52f6f785c2be8b53c30ab16d47
Emanuelle Vodiani <118612780+ManuVodi@users.noreply.github.com>
2024-05-31 09:32:02 -0300

    UPDATE user-route
    Adicionada '/Usuario' nas rotas para diferenciar

--- src/user/list-users/list-user.useCase.ts
+++ src/user/list-users/list-user.useCase.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
 import prismaClient from "../../prisma";
 
-async function listUserUseCase(req: Request ,res: Response) {
+async function listUserUseCase(_: Request ,res: Response) {
     try {
         const listaUsuarios = await prismaClient.usuario.findMany();
         return res.status(200).json(listaUsuarios)
     }
     catch(error){
         console.error(error);
-        return res.status(418).json({error: `Não sei que tipo de erro é. Entrou no useCase para listar os usuários mas não deu boa`})
+        return res.status(418).json({error: `Não foi possível buscar os usuários`})
     }
 }
 
--- src/user/routes/user-Route.ts
+++ src/user/routes/user-Route.ts
@@ -2,7 +2,6 @@ import express from 'express';
 const routeUser = express.Router();
 
 import { createUserController } from '../create-user/create-user.controller';
-import { listUserController } from '../list-users/list-user.controller';
 import { updateUserController } from "../update-user/update-user.controller";
 import { deleteUserController } from '../delete-user/delete-user.controller';
 
@@ -12,10 +11,10 @@ import { updateUserUseCase } from "../update-user/update-user.useCase";
 import { searchUserUseCase } from '../search-user/search-user.useCase';
 import { deleteUserUseCase } from '../delete-user/delete-user.useCase';
 
-routeUser.post("/criarUsuario", createUserController, createUserUseCase);
-routeUser.get("/listarUsuarios", listUserController, listUserUseCase);
-routeUser.patch("/atualizarUsuario/:id", updateUserController, updateUserUseCase);
-routeUser.get("/pesquisarUsuario", searchUserUseCase);
-routeUser.delete("/deletarUsuario/:id", deleteUserController, deleteUserUseCase);
+routeUser.post("/Usuario/criarUsuario", createUserController, createUserUseCase);
+routeUser.get("/Usuario/listarUsuarios", listUserUseCase);
+routeUser.patch("/Usuario/atualizarUsuario/:id", updateUserController, updateUserUseCase);
+routeUser.get("/Usuario/pesquisarUsuario", searchUserUseCase);
+routeUser.delete("/Usuario/deletarUsuario/:id", deleteUserController, deleteUserUseCase);
 
 module.exports = routeUser;
